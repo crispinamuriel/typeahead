4f0c05a9a9caea871aece401fe4011233cd63264
crispinamuriel <crispinamuriel@gmail.com>
2023-05-27 16:37:30 -0400

    fix err

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Typeahead from "./Typeahead";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
